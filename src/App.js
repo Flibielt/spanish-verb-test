89f2b6f98670eee7026f4a0a5a053dcaa167acb5
Csaba Hermann <hcsabi98@gmail.com>
2022-10-02 17:27:42 +0200

    Test form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  function nextVerb() {
+    setIge("estar");
+    setEset("perfecto");
+    setAlany("tú");
+  }
+
+  function checkVerb() {
+    setEredmeny("Helyes");
+  }
+
+  let [ige, setIge] = useState("ser");
+  let [eset, setEset] = useState("jelen");
+  let [alany, setAlany] = useState("yo");
+  let [eredmeny, setEredmeny] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Done!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Eset
+      </p>
+
+      <div>
+        <div>
+          <input type="checkbox" id='jelen' name='Jelen'/> Jelen
+        </div>
+        <div>
+          <input type="checkbox" id='jovo' name='Jövő'/> Jövő
+        </div>
+        <div>
+          <input type="checkbox" id='perfecto' name='Közelmúlt (Pretérito perfecto)'/> Közelmúlt (Pretérito perfecto)
+        </div>
+        <div>
+          <input type="checkbox" id='indefinido' name='Elbeszélő múlt (Pretérito indefinido)'/> Elbeszélő múlt (Pretérito indefinido)
+        </div>
+      </div>
+
+      <p>
+        Ige: {ige}
+      </p>
+      <p>
+        Eset: {eset}
+      </p>
+      <p>
+        Alany: {alany}
+      </p>
+
+      <p>
+        Ragozás: <input type='text'/>
+      </p>
+
+      <p>
+        Eredmény: {eredmeny}
+      </p>
+
+      <p>
+        <button id='ok' onClick={checkVerb}>Ellenőrzés</button>
+        <span> </span>
+        <button id='next' onClick={nextVerb}>Következő</button>
+      </p>
     </div>
   );
 }
